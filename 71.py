0223a3d77fd8d93bc009324cd6c177277787f959
Olga <olyusik.soloveva@mail.ru>
2023-04-20 18:56:39 +0300

    333

--- 71.py
+++ 71.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 class Currency:
 
-    DOLLAR_RUB = 'http://mfd.ru/currency/?currency=USD'
+    dollar = 'http://mfd.ru/currency/?currency=USD'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
 
 
@@ -12,10 +12,12 @@ class Currency:
         self.current_converted_price = float(self.get_currency_price().replace(",", "."))
 
     def get_currency_price1(self):
-        full_page = requests.get(self.DOLLAR_RUB, headers=self.headers)
+        full_page = requests.get(self.dollar, headers=self.headers)
 
-        soup = BeautifulSoup(full_page.content, 'html.parser')
-        return soup
-    print (get_currency_price1)
-
+        soup = BeautifulSoup(full_page.content)
+        dollar = soup.findAll(tag, {'class': attribute_class})
+        dollar_now = float(self.get_currency_price().replace(",", "."))
+        return (dollar_now)
+        print(get_currency_price1(self))
+        
 
