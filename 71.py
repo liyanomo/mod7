a6d9d9f70d14a202d3c7cca051570fc24437402e
Olga <olyusik.soloveva@mail.ru>
2023-04-21 14:54:31 +0300

    333

--- 71.py
+++ 71.py
@@ -5,7 +5,7 @@ class Currency:
 
     dollar = 'http://mfd.ru/currency/?currency=USD'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
-
+    current_converted_price = 0
 
 
     def __init__(self):
@@ -14,10 +14,10 @@ class Currency:
     def get_currency_price1(self):
         full_page = requests.get(self.dollar, headers=self.headers)
 
-        soup = BeautifulSoup(full_page.content)
-        dollar = soup.findAll(tag, {'class': attribute_class})
+        soup = BeautifulSoup(full_page.text, 'html.parser') 
+        dollar = soup.findAll({'class': mfd-u})
         dollar_now = float(self.get_currency_price().replace(",", "."))
         return (dollar_now)
-        print(get_currency_price1(self))
+    print(get_currency_price1())
         
 
