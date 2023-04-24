5f3f336432477279aa850ea187ef2d8cc99704cc
Olga <olyusik.soloveva@mail.ru>
2023-04-24 11:08:31 +0300

    444

--- 71.py
+++ 71.py
@@ -1,3 +1,4 @@
+
 import requests 
 from bs4 import BeautifulSoup 
 
@@ -5,19 +6,23 @@ class Currency:
 
     dollar = 'http://mfd.ru/currency/?currency=USD'
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
-    current_converted_price = 0
-
-
-    def __init__(self):
-        self.current_converted_price = float(self.get_currency_price().replace(",", "."))
-
-    def get_currency_price1(self):
+    current_converted_price = {}
+    def get_currency_price1(self,current_converted_price):
         full_page = requests.get(self.dollar, headers=self.headers)
 
         soup = BeautifulSoup(full_page.text, 'html.parser') 
-        dollar = soup.findAll({'class': mfd-u})
-        dollar_now = float(self.get_currency_price().replace(",", "."))
-        return (dollar_now)
-    print(get_currency_price1())
+        dollar = soup.findAll('table', {'class': 'mfd-table mfd-currency-table'}) 
+        dollar_now = [usd.text for usd in dollar]
+
+        for data in range(1, len(dollar_now), 1):
+            current_converted_price[dollar_now[data]] = dollar_now[data + 1]
+        for i in current_converted_price:
+            current_converted_price[i] = float(current_converted_price.replace(',', '.'))
+        return current_converted_price
+
+c = Currency()
+print(c.get_currency_price1(current_converted_price))
+
+        
         
 
