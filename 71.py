71ef497ef5431c5f4867bcb4286585beb1cd318e
Olga <olyusik.soloveva@mail.ru>
2023-05-02 12:11:18 +0300

    444

--- 71.py
+++ 71.py
@@ -17,8 +17,11 @@ rows = [i.text for i in rows]
 
 date = rows[::3]
 dollar = rows[1::3]
+del dollar[0]
+del date[0]
 dollar_n = dollar.copy()
-del dollar_n [1:]
+del dollar_n [2:]
+del dollar_n [0]
 dollar_now = ''
 for i in dollar_n:
     dollar_now += str(i)
@@ -28,12 +31,13 @@ dollar1 = []
 for i in dollar:
     float_i = float(i)
     dollar1.append(float_i)
-     
+dollar1.reverse()
+date.reverse()
 
 
 x = np.array(dollar1)
 y = np.array([i for i in range(len(date))])
-plt.plot(y, x)
+plt.plot(x, y)
 plt.show()
 
 print('доллар сейчас:', dollar_now)
